a524488f3d3d8fa6e8625f97cee631bb8f67d8ee
Sarthak Sharma <49234866+SarthakSharma-cs18@users.noreply.github.com>
2020-02-24 22:24:27 +0530

    Add files via upload

---
+++ assignment-1-Titanseverus-master/Main.java
@@ -0,0 +1,28 @@
+import menu.Menu;
+public class Main {
+    public static void main(String[] args) {
+        boolean flag = true;
+        Menu menu = new Menu();
+        while (flag) {
+            int n = menu.showChoice();
+            switch (n) {
+                case 1:
+                    menu.addChoice();
+                    break;
+                case 2:
+                    menu.viewChoice();
+                    break;
+                case 3:
+                    menu.searchChoice();
+                    break;
+                case 4:
+                    menu.deleteChoice();
+                    break;
+                case 5:
+                    menu.exitChoice();
+                    flag = false;
+                    break;
+            }
+        }
+    }
+}
